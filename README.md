0077a15f6da0b01b82cacbd2bbbe3147ac8ef3a8
pjchender <pjchender@gmail.com>
2020-10-18 17:17:11 +0800

    add button to toggle web worker feature

--- README.md
+++ README.md
@@ -1,12 +1,11 @@
 # simple web worker example
 
-This repository is forked from [simple-web-worker](https://github.com/mdn/simple-web-worker) @ MDN.
+This repository is modified from [simple-web-worker](https://github.com/mdn/simple-web-worker) @ MDN.
 
-- Provide CPU intensive task (do not use `console.log` as CPU intensive task, cause `console.log` may process in another thread of browser)
-- use `if (window.Worker)` 或 `if (!window.Worker)` to use web worker feature or not.
+- toggle the switch button to enable web worker feature or not.
 
 ## Demo
 
 ```bash
-$ parcel index.html
+$ npm start
 ```
--- main.js
+++ main.js
@@ -4,44 +4,55 @@ const first = document.querySelector('#number1');
 const second = document.querySelector('#number2');
 const result = document.querySelector('.result');
 
-if (!window.Worker) {
-  const myWorker = new Worker(`./worker.js`);
+let enableWebWorker = false;
+const myWorker = new Worker(`./worker.js`);
 
-  first.addEventListener('input', (e) => {
+first.addEventListener('input', (e) => {
+  if (enableWebWorker) {
     myWorker.postMessage([first.value, second.value]);
-  });
-
-  second.addEventListener('input', () => {
-    myWorker.postMessage([first.value, second.value]);
-  });
-
-  myWorker.addEventListener('message', (e) => {
-    result.textContent = e.data;
-  });
-} else {
-  first.addEventListener('input', () => {
+  } else {
     const data = calculate(first.value, second.value);
     result.textContent = data;
-  });
+  }
+});
 
-  second.addEventListener('input', () => {
+second.addEventListener('input', () => {
+  if (enableWebWorker) {
+    myWorker.postMessage([first.value, second.value]);
+  } else {
     const data = calculate(first.value, second.value);
     result.textContent = data;
-  });
-}
-
-first.addEventListener('keydown', (e) => {
-  e.target.style.outlineColor = '#de4463';
+  }
 });
 
-first.addEventListener('keyup', (e) => {
-  e.target.style.outlineColor = 'transparent';
+myWorker.addEventListener('message', (e) => {
+  result.textContent = e.data;
 });
 
-second.addEventListener('keydown', (e) => {
-  e.target.style.outlineColor = '#de4463';
+/* highlight the outline when user input */
+const formElem = document.querySelector('#form');
+formElem.addEventListener('keydown', (e) => {
+  if (e.target && e.target.matches('input')) {
+    e.target.style.outlineColor = '#de4463';
+  }
 });
 
-second.addEventListener('keyup', (e) => {
-  e.target.style.outlineColor = 'transparent';
+formElem.addEventListener('keyup', (e) => {
+  if (e.target && e.target.matches('input')) {
+    e.target.style.outlineColor = 'transparent';
+  }
+});
+
+/* switch button */
+const switchInput = document.querySelector('.switch-input');
+const switchText = document.querySelector('.switch-text');
+switchInput.addEventListener('change', (e) => {
+  const { checked } = e.target;
+  if (checked) {
+    switchText.dataset.text = 'Web Worker On';
+    enableWebWorker = true;
+  } else {
+    switchText.dataset.text = 'Web Worker Off';
+    enableWebWorker = false;
+  }
 });
